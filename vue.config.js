f81a5c65ab12b97530e66f553944c71a17075f05
吕俊伶 <lvjunling@lvjunlingdeMac-mini.local>
2020-09-07 11:48:32 +0800

    opt webpack

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const config = require('./src/config');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const resolve = (dir) => path.join(__dirname, dir);
 const isPro = process.env.NODE_ENV === 'production';
@@ -62,11 +63,38 @@ module.exports = {
       .set('_p', resolve('src/plugins'));
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
     types.forEach((type) => addStyleResource(config.module.rule('stylus').oneOf(type)));
-
+    // 修改html-webpack-plugin,注入到body
     config.plugin('html').tap((args) => {
       const ret = (args || []).map((item) => ({ ...item, inject: 'body' }));
       return ret;
     });
+    // 分割代码
+    config.optimization.splitChunks({
+      cacheGroups: {
+        vendors: {
+          name: 'chunk-vendors',
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          chunks: 'initial',
+          reuseExistingChunk: true
+        },
+        common: {
+          name: 'chunk-common',
+          minChunks: 2,
+          priority: -20,
+          chunks: 'initial',
+          reuseExistingChunk: true
+        },
+        vueBase: {
+          test: (module) => /vue|vuex|vue-router/.test(module.context),
+          chunks: 'initial',
+          name: 'vueBase',
+          priority: 10,
+        }
+      }
+    });
+    // webpack打包分析
+    config.plugin('bundleAnalyzer').use(BundleAnalyzerPlugin, []);
   },
 
   productionSourceMap: false,
